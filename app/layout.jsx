1bf25c3c9493ae6ddff9baa7c34be7c3139e5c4a
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2024-08-11 12:15:25 +0400

    made the initial commits for my first next.js project !

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, real estate, property',
+  description: 'Find the perfect rental property',
+};
+
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default MainLayout;
